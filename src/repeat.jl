4e193611a7cb812a06a9b09a35e8f99421418dfa
Gudongyangg <1085503343@qq.com>
2021-11-15 20:08:25 +0800

    加入key

--- src/repeat.jl
+++ src/repeat.jl
@@ -1,5 +1,35 @@
 module repeat
 
 # Write your package code here.
+"""
+    function Simpson(f, a, b)
 
+Calculate the integral ``f`` function from ``x`` to ``y`` by Simpson formula
+
+Here's Simpson expression:
+
+``Simpson(f, a, b) = \\frac{(b-a)}{6} * (f(a) + 4*f(\\frac{(a+b)}{2}) + f(b))``
+
+# Arguments
+- `f::Express`: integrand function
+- `a::Float`: left boundary of integral interval of definite integral
+- `b::Float`: right boundary of integral interval of definite integral
+
+
+# Examples
+```julia-repl
+julia> Simpson(2*x, 1, 2)
+4
+```
+"""
+function Simpson(f, a, b)
+    # Simpson Formula
+    # (b-a)/6 * (f(a) + 4*f((a+b)/2) + f(b))
+    h = (b - a)/2
+    x = a:h:b
+    sum((x[3] - x[1])/6*(f(x[3]) + 4*f(x[2]) + f(x[1])))
+end
+
+function g(x)
+    2*x
 end
